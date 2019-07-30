817bf3be84729aed02c5141951315cb7351aaa91
Jarosław Gosławski <jarek93pl@hotmail.com>
2019-07-30 12:34:34 +0200

    Dodaj pliki projektów.

---
+++ KursHtml/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KursHtml
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
